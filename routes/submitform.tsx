5e908f755948e7225de3ad6b1747e3e16bbdd9d3
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2024-03-08 17:29:39 +0100

    first commit

---
+++ routes/submitform.tsx
@@ -0,0 +1,39 @@
+import { FreshContext, Handlers } from "$fresh/server.ts";
+import Form from "../islands/Form.tsx";
+import ContactModel from "../db/Contact.ts";
+
+export const handler: Handlers = {
+  POST: async (req: Request, ctx: FreshContext) => {
+    try {
+      const form = await req.formData();
+      const data = {
+        name: form.get("name"),
+        age: form.get("age"),
+        email: form.get("email"),
+      };
+
+      await ContactModel.create(data);
+
+      return new Response("", {
+        status: 303,
+        headers: {
+          "Location": "/",
+        },
+      });
+    } catch (error) {
+      return new Response(error.message, {
+        status: 500,
+      });
+    }
+  },
+};
+
+const submitform = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default submitform;
