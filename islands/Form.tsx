ed02f32f0bd9d34820b2ef9698cf8f4eda96b002
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2024-03-08 17:32:37 +0100

    first commit

--- islands/Form.tsx
+++ islands/Form.tsx
@@ -32,7 +32,7 @@ export const Form: FunctionComponent = () => {
     <div class="form">
       <h1>Introduce tus datos</h1>
       <form
-        action="/submitform.tsx"
+        action="../submitform.tsx"
         method="POST"
         onSubmit={submitHandler}
       >
